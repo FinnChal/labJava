64d85301dfa50fb1dd508a5eaa9594ec34184ac0
FinnChal <Shine.C@kkumail.com>
2022-09-02 14:46:04 +0700

    version 1

---
+++ labJava.java
@@ -0,0 +1,18 @@
+public class labJava {
+    /* f(x) = f(x) * f(x-1) * ... * 1 */
+    public static int f( int x ) {
+        if (x <= 1) {
+            System.out.println(x);
+            return 1;
+        }
+        else
+            System.out.println(x);
+            return ((x) * f(x-1));
+    }
+
+    public static void main(String [ ] args) {
+        int x = 5;
+        System.out.println( "f(" + x +") = "+ f(x));
+        System.out.println("This is Java code exmaple");
+    }
+}
